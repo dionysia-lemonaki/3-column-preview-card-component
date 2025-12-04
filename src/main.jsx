d280a578156651361f0f39b2b620a253e9cf3fb0
dionysia-lemonaki <105449958+dionysia-lemonaki@users.noreply.github.com>
2025-12-04 15:46:27 +0200

    Add tokens

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import data from "./data";
+import "./index.css";
 
 createRoot(document.querySelector("#root")).render(
   <StrictMode>
