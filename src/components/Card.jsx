3b0e23b98b6d1a51df78d0aafa05209ee8e53c10
dionysia-lemonaki <105449958+dionysia-lemonaki@users.noreply.github.com>
2025-12-04 17:43:50 +0200

    Add Card styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,10 +2,10 @@ import Button from "./Button";
 
 const Card = ({ variant, image, title, description }) => {
   return (
-    <div className={`card card--${variant}`}>
+    <div className={`card card--${variant} flex-column`}>
       <img src={image} alt="" width="64" height="40" className="card__image" />
-      <div className="card__content">
-        <div className="card__intro">
+      <div className="card__content flex-column">
+        <div className="card__intro flex-column">
           <h2 className="card__title">{title}</h2>
           <p className="card__description">{description}</p>
         </div>
