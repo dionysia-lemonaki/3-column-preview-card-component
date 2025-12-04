f14350e14fee8ab7d641e91d7dd62f072732696b
dionysia-lemonaki <105449958+dionysia-lemonaki@users.noreply.github.com>
2025-12-04 13:49:49 +0200

    Add card data

---
+++ src/data.js
@@ -0,0 +1,32 @@
+import iconSedans from "./assets/images/icon-sedans.svg";
+import iconSuvs from "./assets/images/icon-suvs.svg";
+import iconLuxury from "./assets/images/icon-luxury.svg";
+
+const data = [
+  {
+    id: 1,
+    variant: "sedans",
+    image: iconSedans,
+    title: "Sedans",
+    description:
+      "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+  },
+  {
+    id: 2,
+    variant: "suvs",
+    image: iconSuvs,
+    title: "SUVs",
+    description:
+      "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
+  },
+  {
+    id: 3,
+    variant: "luxury",
+    image: iconLuxury,
+    title: "Luxury",
+    description:
+      "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style. ",
+  },
+];
+
+export default data;
